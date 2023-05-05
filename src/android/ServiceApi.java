9b6be5c26bddd1bff8486946ec663af4f9319a16
serhan.maknik <maqnik@gmail.com>
2023-05-05 22:11:48 +0300

    ServiceApi updated

--- src/android/ServiceApi.java
+++ src/android/ServiceApi.java
@@ -12,8 +12,15 @@ import retrofit2.http.POST;
 import retrofit2.http.Url;
 
 public interface ServiceApi {
+    @POST
+    Call<ResponseBody> postData(@Url String url);
 
+    @POST
+    Call<ResponseBody> postData(@Url String url, @Body String body );
 
-     @POST
-    Call<ResponseBody> postData(@Url String url, @HeaderMap Map<String, String> header, @Body String body );
+    @POST
+    Call<ResponseBody> postData(@Url String url, @HeaderMap Map<String, String> header );
+
+    @POST
+    Call<ResponseBody> postData(@Url String url, @HeaderMap Map<String, String> header,@Body String body );
 }
